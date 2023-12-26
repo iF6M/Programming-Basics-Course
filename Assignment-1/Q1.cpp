ee2ed0cde6ba0ba7bd70c1d55469c94d43650eb8
Fatimah Al-Musawi <f.almusawi37@gmail.com>
2023-12-26 21:29:06 +0300

    Add files

---
+++ Assignment-1/Q1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numberOne = 0;
+    int numberTwo = 0;
+    cout << "Enter number one: ";
+    cin >> numberOne;
+    cout << endl << "Enter number two: ";
+    cin >> numberTwo;
+    cout << endl << "Result: " << ((numberOne + numberTwo)*3)-10 << endl;
+}
